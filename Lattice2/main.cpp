5451bc2c50be8c7437a2b409e93fbee5af7b707f
Oliver Spiro <Oliver@olivers-macbook-air.local>
2016-05-21 13:00:58 -0400

    Initial Commit

---
+++ Lattice2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lattice2
+//
+//  Created by Oliver Spiro on 5/21/16.
+//  Copyright © 2016 Oliver Spiro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
