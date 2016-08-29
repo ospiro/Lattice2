d11f39f346b27324ad03d01f587e036a22e7a306
ospiro <Oliver@1x-nat-vl930-172-30-64-209.wireless.umass.edu>
2016-08-29 14:47:50 -0400

    fixed casting

--- Lattice2/Lattice.cpp
+++ Lattice2/Lattice.cpp
@@ -113,7 +113,7 @@ Lattice::Lattice(int setWidth,
     {
         for(int j = -radius; j <=radius; j++)
         {
-            if((double(i)*double(i) + double(j)*double(j) <= radius*radius) && (i*i + j*j > 0))
+            if((i*i+j*j <= radius*radius) && (i*i + j*j > 0))
             {
                 count++;
                 stepI.push_back(i);
